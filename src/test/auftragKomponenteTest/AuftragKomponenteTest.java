34545bf07614c19aca5f8ca49cdfb4fee4000337
Flo <florian@kletz.de>
2014-04-14 15:07:18 +0200

    hibernate add

---
+++ src/test/auftragKomponenteTest/AuftragKomponenteTest.java
@@ -0,0 +1,29 @@
+package test.auftragKomponenteTest;
+
+import static org.junit.Assert.*;
+
+import org.hibernate.type.DateType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import auftragKomponente.dataAccessLayer.Auftrag;
+
+public class AuftragKomponenteTest {
+
+	@Before
+	public void setUp() throws Exception {
+		Auftrag a = new Auftrag();
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
